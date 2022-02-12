95b5c3e3e237a88ce5747cb0188daa527aa73322
red <red.avtovo@gmail.com>
2022-02-12 17:40:07 +0100

    refactored skyeng client

--- src/lib/skyeng.rs
+++ src/lib/skyeng.rs
@@ -3,9 +3,8 @@ use std::time::UNIX_EPOCH;
 use log::info;
 use reqwest::Client;
 use scraper::Html;
-use crate::lib::errors::Error::ServerError;
+use crate::lib::errors::Error::{ServerError, UserError};
 use serde::Deserialize;
-use async_trait::async_trait;
 
 use super::errors::{
     Error,
@@ -13,10 +12,29 @@ use super::errors::{
     Result,
 };
 
-#[derive(Debug)]
+pub(crate) fn curr_millis() -> u128 {
+    let now = std::time::SystemTime::now();
+    let since_the_epoch = now.duration_since(UNIX_EPOCH).unwrap();
+    since_the_epoch.as_millis()
+}
+
+#[derive(Debug, Clone)]
 pub struct Token {
     value: String,
     expires: u128,
+}
+
+trait Expirable {
+    fn is_expired(&self) -> bool;
+}
+
+impl Expirable for Option<Token> {
+    fn is_expired(&self) -> bool {
+        match self {
+            Some(token) => token.expires < curr_millis(),
+            None => true,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -55,135 +73,194 @@ pub struct Words {
     pub data: Vec<WordData>,
 }
 
-pub fn get_client() -> Client {
-    reqwest::Client::builder().cookie_store(true).build().unwrap()
+#[derive(Debug, Clone)]
+struct Skyeng {
+    client: Client,
+    token: Option<Token>,
+    user: String,
+    password: String,
 }
 
-/// Calls login endpoint and gets csrf token + session cookies
-async fn get_csrf(client: &Client) -> Result<String> {
-    let path = "https://id.skyeng.ru/login".to_string();
-    let res = client.get(&path).send().await.map_err(|e| Error::Reqwest { e, path })?;
+impl Skyeng {
+    /// Calls login endpoint and gets csrf token + session cookies
+    pub(crate) async fn get_csrf(&self) -> Result<String> {
+        let path = "https://id.skyeng.ru/login".to_string();
+        let res = self.client.get(&path).send().await.map_err(|e| Error::Reqwest { e, path })?;
 
-    let response = res.text().await?;
-    let doc = Html::parse_document(&response);
-    let selector = scraper::Selector::parse("input[name=csrfToken]")
-        .map_err(|_| HttpParsingError { message: "Unable to parse the page with csrf token" })?;
-    let csrf = doc
-        .select(&selector)
-        .next()
-        .ok_or("Unable to find csrf element on the page")
-        .and_then(|v| v
-            .value()
-            .attr("value")
-            .ok_or("Unable to find value of csrf token element")
-            .map(|v| v.to_string())
-        );
+        let response = res.text().await?;
+        let doc = Html::parse_document(&response);
+        let selector = scraper::Selector::parse("input[name=csrfToken]")
+            .map_err(|_| HttpParsingError { message: "Unable to parse the page with csrf token" })?;
+        let csrf = doc
+            .select(&selector)
+            .next()
+            .ok_or("Unable to find csrf element on the page")
+            .and_then(|v| v
+                .value()
+                .attr("value")
+                .ok_or("Unable to find value of csrf token element")
+                .map(|v| v.to_string())
+            );
 
-    csrf.map_err(|e| HttpParsingError { message: e })
-}
-
-fn curr_millis() -> u128 {
-    let now = std::time::SystemTime::now();
-    let since_the_epoch = now.duration_since(UNIX_EPOCH).unwrap();
-    since_the_epoch.as_millis()
-}
-
-async fn get_jwt(client: &Client) -> Result<Token> {
-    let path = "https://id.skyeng.ru/user-api/v1/auth/jwt".to_string();
-    let res = client.post(&path).send().await.map_err(|e| Error::Reqwest { e, path })?;
-    let mut cookies = res.cookies();
-    cookies.next()
-        .ok_or_else(|| ServerError { message: "jwt token was not found in response "})
-        .map(|v| Token {
-            value: v.value().to_string(),
-            expires: v.expires().unwrap()
-                .duration_since(UNIX_EPOCH)
-                .expect("Time went backwards").as_millis()
-        })
-}
-
-async fn login(user: &String, password: &String) -> Result<Token> {
-    let client = reqwest::Client::builder()
-        .cookie_store(true).build().unwrap();
-    let csrf = get_csrf(&client).await?;
-    let path = "https://id.skyeng.ru/frame/login-submit".to_string();
-    let mut params = HashMap::new();
-        params.insert("username", user);
-        params.insert("password", password);
-        params.insert("csrfToken", &csrf);
-    info!("Logging in user {}", user);
-    let rs = client.post(&path)
-        .form(&params)
-        .send().await
-        .map_err(|e| Error::Reqwest { e, path })?;
-
-    if rs.status().is_server_error() {
-        return Err(Error::ServerError { message: "Bad credentials" });
+        csrf.map_err(|e| HttpParsingError { message: e })
     }
-    if rs.status().is_client_error() {
-        return Err(Error::UserError { message: "Bad credentials" });
-    }
-    get_jwt(&client).await
-}
 
-async fn get_word_sets(token: &Token, student_id: u32) -> Result<Vec<WordSetData>> {
-    let path = "https://api.words.skyeng.ru/api/for-vimbox/v1/wordsets.json".to_string();
-    let page_size = 100;
-    let client = reqwest::Client::new();
-    let mut current_page = 1;
-    let mut word_sets = Vec::new();
-    loop {
-        info!("Calling {} page {}", path, current_page);
+    async fn get_jwt(&self) -> Result<Token> {
+        let path = "https://id.skyeng.ru/user-api/v1/auth/jwt".to_string();
+        let res = self.client.post(&path).send().await.map_err(|e| Error::Reqwest { e, path })?;
+        let mut cookies = res.cookies();
+        cookies.next()
+            .ok_or_else(|| ServerError { message: "jwt token was not found in response " })
+            .map(|v| Token {
+                value: v.value().to_string(),
+                expires: v.expires().unwrap()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time went backwards").as_millis(),
+            })
+    }
+
+    pub async fn login(&mut self) -> Result<&mut Self> {
+        let csrf = self.get_csrf().await?;
+        let path = "https://id.skyeng.ru/frame/login-submit".to_string();
         let mut params = HashMap::new();
-        params.insert("page", &current_page);
-        params.insert("pageSize", &page_size);
-        params.insert("studentId", &student_id);
-        let res = client.get(&path)
-            .query(&params)
-            .bearer_auth(&token.value)
-            .send().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
-        let response = res.json::<WordSet>().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
-        word_sets.extend(response.data);
-        if response.meta.current_page == response.meta.last_page {
-            break;
-        }
-        current_page += 1;
-    }
-    Ok(word_sets)
-}
+        params.insert("username", &self.user);
+        params.insert("password", &self.password);
+        params.insert("csrfToken", &csrf);
+        info!("Logging in user {}", &self.user);
+        let rs = self.client.post(&path)
+            .form(&params)
+            .send().await
+            .map_err(|e| Error::Reqwest { e, path })?;
 
-pub async fn get_words(token: &Token, student_id: u32) -> Result<Vec<WordData>> {
-    let word_sets = get_word_sets(token, student_id).await?;
-    let mut words = Vec::new();
-    let client = reqwest::Client::new();
-    let page_size = "100".to_string();
-    let accepted_language = "ru".to_string();
-    let curr_time = curr_millis().to_string();
-    for word_set in word_sets {
-        let path = format!("https://api.words.skyeng.ru/api/v1/wordsets/{}/words.json", word_set.id);
+        if rs.status().is_server_error() {
+            return Err(Error::ServerError { message: "Bad credentials" });
+        }
+        if rs.status().is_client_error() {
+            return Err(Error::UserError { message: "Bad credentials" });
+        }
+        let token = self.get_jwt().await?;
+        self.token = Some(token);
+        Ok(self)
+    }
+
+    async fn get_word_sets(&mut self, student_id: u32) -> Result<Vec<WordSetData>> {
+        let path = "https://api.words.skyeng.ru/api/for-vimbox/v1/wordsets.json".to_string();
+        let page_size = 100;
         let mut current_page = 1;
+        let mut word_sets = Vec::new();
         loop {
             info!("Calling {} page {}", path, current_page);
-            let res = client.get(&path)
+            let mut params = HashMap::new();
+            params.insert("page", &current_page);
+            params.insert("pageSize", &page_size);
+            params.insert("studentId", &student_id);
+            let token = self.get_fresh_token().await.ok_or_else(|| UserError { message: "Token is not set. Unable to login" })?;
+            let res = self.client.get(&path)
+                .query(&params)
                 .bearer_auth(&token.value)
-                .query(&[
-                    ("page", &current_page.to_string()),
-                    ("pageSize", &page_size),
-                    ("studentId", &student_id.to_string()),
-                    ("acceptLanguage", &accepted_language),
-                    ("noCache", &curr_time),
-                ])
                 .send().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
-            let response = res.json::<Words>().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
-            words.extend(response.data);
+            let response = res.json::<WordSet>().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
+            word_sets.extend(response.data);
             if response.meta.current_page == response.meta.last_page {
                 break;
             }
             current_page += 1;
         }
+        Ok(word_sets)
     }
-    Ok(words)
+
+    pub async fn get_words(&mut self, student_id: u32) -> Result<Vec<WordData>> {
+        let word_sets = self.get_word_sets(student_id).await?;
+        let mut words = Vec::new();
+        let page_size = "100".to_string();
+        let accepted_language = "ru".to_string();
+        let curr_time = curr_millis().to_string();
+        let token = self.get_fresh_token().await.ok_or_else(|| UserError { message: "Token is not set. Unable to login" })?;
+        for word_set in word_sets {
+            let path = format!("https://api.words.skyeng.ru/api/v1/wordsets/{}/words.json", word_set.id);
+            let mut current_page = 1;
+            loop {
+                info!("Calling {} page {}", path, current_page);
+                let res = self.client.get(&path)
+                    .bearer_auth(&token.value)
+                    .query(&[
+                        ("page", &current_page.to_string()),
+                        ("pageSize", &page_size),
+                        ("studentId", &student_id.to_string()),
+                        ("acceptLanguage", &accepted_language),
+                        ("noCache", &curr_time),
+                    ])
+                    .send().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
+                let response = res.json::<Words>().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
+                words.extend(response.data);
+                if response.meta.current_page == response.meta.last_page {
+                    break;
+                }
+                current_page += 1;
+            }
+        }
+        Ok(words)
+    }
+
+    #[allow(dead_code)]
+    pub fn get_token(&self) -> Option<Token> {
+        match self.token.as_ref() {
+            Some(token) => Some(token.clone()),
+            None => None,
+        }
+    }
+
+    async fn get_fresh_token(&mut self) -> Option<Token> {
+        match if self.token.is_expired() {
+            self.login().await
+        } else {
+            Ok(self)
+        }  {
+            Ok(v) => v.get_token(),
+            _ => None
+        }
+
+    }
+
+    fn client() -> Client {
+        reqwest::Client::builder().cookie_store(true).build().unwrap()
+    }
+
+    pub fn new(user: String, password: String) -> Self {
+        Self {
+            client: Self::client(),
+            token: None,
+            user,
+            password,
+        }
+    }
+
+    pub fn new_with_token(token: Option<Token>, user: String, password: String) -> Self {
+        Self {
+            client: Self::client(),
+            token,
+            user,
+            password,
+        }
+    }
+
+    async fn get_meanings(&mut self, words: &Vec<WordData>) -> Result<Vec<Meaning>> {
+        let path = "https://dictionary.skyeng.ru/api/for-services/v2/meanings".to_string();
+        let ids = words.into_iter()
+            .map(|w| w.meaning_id.to_string())
+            .collect::<Vec<String>>()
+            .join(",");
+        let token = self.get_fresh_token().await.ok_or_else(|| UserError { message: "Token is not set. Unable to login" })?;
+        let res = self.client.get(&path)
+            .bearer_auth(&token.value)
+            .query(&[("ids", &ids)])
+            .send().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
+        let body = String::from_utf8(res.bytes().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?.to_vec()).unwrap();
+        serde_json::from_str(&body)
+            .map_err(|e| Error::DeserializationError { e, message: body.clone() })
+    }
 }
+
 
 #[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
@@ -196,7 +273,7 @@ struct Meaning {
     pub sound_url: String,
     pub text: String,
     pub transcription: String,
-    pub translation: Translation
+    pub translation: Translation,
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -223,14 +300,11 @@ struct MeaningImage {
     pub url: String,
 }
 
-#[async_trait]
 trait NewWords {
     fn created_after(self, date_time: String) -> Vec<WordData>;
     fn last_created(&self) -> Option<String>;
-    async fn get_meanings(&self, token: &Token) -> Result<Vec<Meaning>>;
 }
 
-#[async_trait]
 impl NewWords for Vec<WordData> {
     fn created_after(self, date_time: String) -> Vec<WordData> {
         self.iter().filter(|w| w.created_at > date_time).cloned().collect()
@@ -242,22 +316,6 @@ impl NewWords for Vec<WordData> {
             .max()
             .map(|w| w.to_string())
     }
-
-    async fn get_meanings(&self, token: &Token) -> Result<Vec<Meaning>> {
-        let path = "https://dictionary.skyeng.ru/api/for-services/v2/meanings".to_string();
-        let client = reqwest::Client::new();
-        let ids = self.into_iter()
-            .map(|w| w.meaning_id.to_string())
-            .collect::<Vec<String>>()
-            .join(",");
-        let res = client.get(&path)
-            .bearer_auth(&token.value)
-            .query(&[("ids", &ids)])
-            .send().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?;
-        let body = String::from_utf8(res.bytes().await.map_err(|e| Error::Reqwest { e, path: path.clone() })?.to_vec()).unwrap();
-        serde_json::from_str(&body)
-            .map_err(|e| Error::DeserializationError { e, message: body.clone() })
-    }
 }
 
 #[cfg(test)]
@@ -268,9 +326,8 @@ mod test {
 
     #[tokio::test]
     pub async fn test_csrf() {
-        let client = reqwest::Client::builder()
-            .cookie_store(true).build().unwrap();
-        let result = get_csrf(&client).await;
+        let skyeng = Skyeng::new("".to_string(), "".to_string());
+        let result = skyeng.get_csrf().await;
         println!("result: {:#?}", result);
         assert!(result.is_ok());
         let csrf = result.unwrap();
@@ -278,12 +335,12 @@ mod test {
         assert!(csrf.len() > 0)
     }
 
-    async fn token() -> Token {
-        let result = login(
-            &"red.avtovo@gmail.com".to_string(),
-            &env::var("PASS").expect("User password expected")
-        ).await;
-        result.unwrap()
+    async fn skyeng() -> Skyeng {
+        Skyeng::new_with_token(
+            None,
+            "red.avtovo@gmail.com".to_string(),
+            env::var("PASS").expect("User password expected"),
+        ).login().await.unwrap().clone()
     }
 
     #[tokio::test]
@@ -292,34 +349,39 @@ mod test {
     //   expires: 1644763320000,
     // }
     pub async fn test_login() {
-        let result = login(
-            &"red.avtovo@gmail.com".to_string(),
-            &env::var("PASS").expect("User password expected")
-        ).await;
-        println!("result: {:#?}", result);
-        assert!(result.unwrap().value.len() > 0);
+        let mut skyeng = Skyeng::new(
+            "red.avtovo@gmail.com".to_string(),
+            env::var("PASS").expect("User password expected"),
+        );
+        let result = skyeng.login().await;
+        assert!(result.is_ok());
+        let option = &result.unwrap().token;
+        println!("result: {:#?}", option);
+        assert!(option.is_some());
+        let token = option.as_ref().unwrap();
+        assert!(token.value.len() > 0);
     }
 
     #[test(tokio::test)]
     pub async fn test_get_word_sets() {
-        let token = token().await;
-        let result = get_word_sets(&token,6605911).await;
+        let mut skyeng = skyeng().await;
+        let result = skyeng.get_word_sets(6605911).await;
         println!("result: {:#?}", result);
         assert!(result.is_ok());
     }
 
     #[tokio::test]
     pub async fn test_get_words() {
-        let token = token().await;
-        let result = get_words(&token,6605911).await;
+        let mut skyeng = skyeng().await;
+        let result = skyeng.get_words(6605911).await;
         assert!(result.is_ok());
         println!("result: {}", result.unwrap().len());
     }
 
     #[tokio::test]
     pub async fn test_get_new_words() {
-        let token = token().await;
-        let result = get_words(&token, 6605911).await.map(
+        let mut skyeng = skyeng().await;
+        let result = skyeng.get_words(6605911).await.map(
             |words| words.created_after(
                 "2022-01-01T00:00:00.000Z".to_string()
             )
@@ -330,8 +392,8 @@ mod test {
 
     #[tokio::test]
     pub async fn test_get_last_created_words() {
-        let token = token().await;
-        let result = get_words(&token, 6605911).await
+        let mut skyeng = skyeng().await;
+        let result = skyeng.get_words(6605911).await
             .unwrap()
             .last_created();
         assert!(result.is_some());
@@ -340,14 +402,14 @@ mod test {
 
     #[tokio::test]
     pub async fn test_get_meanings() {
-        let token = token().await;
-        let result = get_words(&token, 6605911).await
+        let mut skyeng = skyeng().await;
+        let words = skyeng.get_words(6605911).await
             .unwrap()
             .iter().rev()
             .take(2)
             .map(|v| v.clone())
-            .collect::<Vec<WordData>>()
-            .get_meanings(&token).await;
+            .collect::<Vec<WordData>>();
+        let result = skyeng.get_meanings(&words).await;
         println!("result: {:#?}", result);
         assert!(result.is_ok());
         assert!(result.unwrap().len() >= 2);
