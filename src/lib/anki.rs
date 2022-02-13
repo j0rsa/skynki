a460e29e766cbdfee799d6a84b647c5e3d6f7055
red <red.avtovo@gmail.com>
2022-02-13 20:10:28 +0100

    add anki note

--- src/lib/anki.rs
+++ src/lib/anki.rs
@@ -5,7 +5,48 @@ use serde::Serialize;
 struct Action {
     pub version: String,
     pub action: String,
-    pub data: Option<serde_json::Value>,
+    pub params: Option<Note>,
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Note {
+    pub deck_name: String,
+    pub model_name: String,
+    pub fields: Fields,
+    pub options: Options,
+    pub tags: Vec<String>,
+    pub audio: Option<Attachment>,
+    pub picture: Option<Attachment>,
+}
+
+#[derive(Serialize, Debug)]
+struct Fields {
+    #[serde(rename = "Text")]
+    pub text: String,
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Options{
+    allow_duplicate: bool, //false
+    duplicate_scope: String, //deck
+    duplicate_scope_options: Option<DuplicateScopeOptions>,
+
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct DuplicateScopeOptions {
+    pub deck_name: String, //"Default"
+    pub check_children: bool, //false
+    pub check_all_models: bool, //false
+}
+
+struct Attachment {
+    pub url: String,
+    pub filename: String,
+    pub fields: Vec<String>, //[Extra]
 }
 
 pub struct Anki {
@@ -25,7 +66,21 @@ impl Anki {
         let data = Action {
             version: "6".to_string(),
             action: "sync".to_string(),
-            data: None,
+            params: None,
+        };
+        let path = self.url.clone();
+        self.client.post(&path)
+            .json(&data)
+            .send().await
+            .map(|_| ())
+            .map_err(|e| Error::Reqwest { e, path })
+    }
+
+    pub async fn add_note(&self, note: Note) -> Result<()> {
+        let data = Action {
+            version: "6".to_string(),
+            action: "addNote".to_string(),
+            params: Some(note),
         };
         let path = self.url.clone();
         self.client.post(&path)
